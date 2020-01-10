228546780416f73b2ca899ac4a3d162629ecdac5
David Atkins <atki1080@gmail.com>
2020-01-10 11:26:02 -0700

    Add attributes test

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -2,10 +2,18 @@ require_relative "test_helper"
 require "./lib/enigma"
 
 class EnigmaTest < Minitest::Test
-  
-  def test_it_exists
-    enigma = Enigma.new
 
-    assert_instance_of Enigma, enigma
+  def setup
+    @enigma = Enigma.new
+  end
+
+  def test_it_exists
+    assert_instance_of Enigma, @enigma
+  end
+
+  def test_it_has_attributes
+    assert_instance_of EnigmaEncoder, @enigma.encoder
+    assert_instance_of EnigmaDecoder, @enigma.decoder
+    assert_instance_of EnigmaCracker, @enigma.cracker
   end
 end
