e5c7cd1fa1182f9ac566962755a944caeb657d35
David Atkins <atki1080@gmail.com>
2020-01-10 10:15:56 -0700

    Add EnigmaEncoder test suite

---
+++ test/enigma_encoder_test.rb
@@ -0,0 +1,10 @@
+require_relative "test_helper"
+require "./lib/enigma_encoder"
+
+class EnigmaEncoderTest < Minitest::Test
+  def test_it_exists
+    encoder = EnigmaEncoder.new
+
+    assert_instance_of EnigmaEncoder, encoder
+  end
+end
