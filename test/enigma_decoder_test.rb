a934dbaa5e3e48a527b9d58d662df6112359b433
David Atkins <atki1080@gmail.com>
2020-01-10 10:18:31 -0700

    Add EnigmaDecoder test suite

---
+++ test/enigma_decoder_test.rb
@@ -0,0 +1,10 @@
+require_relative "test_helper"
+require "./lib/enigma_decoder"
+
+class EnigmaDecoderTest < Minitest::Test
+  def test_it_exists
+    decoder = EnigmaDecoder.new
+
+    assert_instance_of EnigmaDecoder, decoder
+  end
+end
