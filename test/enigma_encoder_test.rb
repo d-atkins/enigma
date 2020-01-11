8fa4a3fedc7ce34c832265b90321fcb823b5a31b
David Atkins <atki1080@gmail.com>
2020-01-10 21:47:36 -0700

    Remove key_array test

--- test/enigma_encoder_test.rb
+++ test/enigma_encoder_test.rb
@@ -12,15 +12,10 @@ class EnigmaEncoderTest < Minitest::Test
   end
 
   def test_it_can_set_keys
-    assert_equal "02277115", @encoder.keys("02715")
+    assert_equal ["02", "27", "71", "15"], @encoder.keys("02715")
   end
 
   def test_it_can_set_offset
-    assert_equal "1025", @encoder.offset("040895")
-  end
-
-  def test_it_can_make_key_array
-    assert_equal ['1', '2', '3', '4'], @encoder.key_array("1234")
-    assert_equal ['15', '26', '37', '48'], @encoder.key_array("15263748")
+    assert_equal ["1", "0", "2", "5"], @encoder.offset("040895")
   end
 end
