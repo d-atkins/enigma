d36340853a11b2929a32ed36927437bdcd9217f8
David Atkins <atki1080@gmail.com>
2020-01-10 21:12:55 -0700

    Add keys test

--- test/enigma_encoder_test.rb
+++ test/enigma_encoder_test.rb
@@ -2,10 +2,16 @@ require_relative "test_helper"
 require "./lib/enigma_encoder"
 
 class EnigmaEncoderTest < Minitest::Test
-  
-  def test_it_exists
-    encoder = EnigmaEncoder.new
 
-    assert_instance_of EnigmaEncoder, encoder
+  def setup
+    @encoder = EnigmaEncoder.new
+  end
+
+  def test_it_exists
+    assert_instance_of EnigmaEncoder, @encoder
+  end
+
+  def test_it_can_set_keys
+    assert_equal "02277115", @encoder.keys("02715")
   end
 end
