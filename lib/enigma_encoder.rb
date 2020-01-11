243b1a1a1472fcd9ca2e0dc2172d52c0957682bd
David Atkins <atki1080@gmail.com>
2020-01-10 21:48:38 -0700

    Change keys and offset methods to return arrays

--- lib/enigma_encoder.rb
+++ lib/enigma_encoder.rb
@@ -1,6 +1,10 @@
 class EnigmaEncoder
 
   def keys(key)
-    key.split('').each_cons(2).map {|a,b| a + b}.join
+    key.split('').each_cons(2).map {|a,b| a + b}
+  end
+
+  def offset(date)
+    (date.to_i**2).to_s[-4..-1].split('')
   end
 end
