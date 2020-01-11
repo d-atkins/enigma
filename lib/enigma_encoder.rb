a10c2aeeecf5c5363863286755d917ee8a319e58
David Atkins <atki1080@gmail.com>
2020-01-10 21:57:55 -0700

    Add shift method

--- lib/enigma_encoder.rb
+++ lib/enigma_encoder.rb
@@ -7,4 +7,10 @@ class EnigmaEncoder
   def offset(date)
     (date.to_i**2).to_s[-4..-1].split('')
   end
+
+  def shift(keys, offset)
+    keys.map.with_index do |key, index|
+      key.to_i + offset[index].to_i
+    end
+  end
 end
