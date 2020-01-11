ab71cb37787c4eb64b5223847410adf2d9245b2b
David Atkins <atki1080@gmail.com>
2020-01-10 22:17:03 -0700

    Add caesar_cipher_shift method

--- lib/enigma_encoder.rb
+++ lib/enigma_encoder.rb
@@ -1,5 +1,9 @@
 class EnigmaEncoder
 
+  def initialize
+    @whitelist = ('a'..'z').to_a << ' '
+  end
+
   def keys(key)
     key.split('').each_cons(2).map {|a,b| a + b}
   end
@@ -8,9 +12,14 @@ class EnigmaEncoder
     (date.to_i**2).to_s[-4..-1].split('')
   end
 
-  def shift(keys, offset)
+  def shifts(keys, offset)
     keys.map.with_index do |key, index|
       key.to_i + offset[index].to_i
     end
+  end
+
+  def caesar_cipher_shift(character, shift)
+    index = @whitelist.index(character.downcase)
+    index ? @whitelist.rotate(shift)[index] : character
   end
 end
