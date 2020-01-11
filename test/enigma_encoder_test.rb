d3530d5587ac08616e6ffc6f051a399f43a79a6f
David Atkins <atki1080@gmail.com>
2020-01-10 22:07:16 -0700

    Add caesar_cipher_shift test

--- test/enigma_encoder_test.rb
+++ test/enigma_encoder_test.rb
@@ -19,10 +19,17 @@ class EnigmaEncoderTest < Minitest::Test
     assert_equal ["1", "0", "2", "5"], @encoder.offset("040895")
   end
 
-  def test_it_can_set_shift
+  def test_it_can_set_shifts
     keys = @encoder.keys("02715")
     offset = @encoder.offset("040895")
 
-    assert_equal [3, 27, 73, 20], @encoder.shift(keys, offset)
+    assert_equal [3, 27, 73, 20], @encoder.shifts(keys, offset)
+  end
+
+  def test_it_can_caesar_cipher_shift
+    assert_equal "b", @encoder.caesar_cipher_shift("a", 1)
+    assert_equal "d", @encoder.caesar_cipher_shift("D", 3)
+    assert_equal " ", @encoder.caesar_cipher_shift("a", -1)
+    assert_equal "a", @encoder.caesar_cipher_shift("a", 54)
   end
 end
