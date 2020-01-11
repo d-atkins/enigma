1dae1f47b80de8def543361f4195705d30dd9dec
David Atkins <atki1080@gmail.com>
2020-01-10 21:53:34 -0700

    Add shift test

--- test/enigma_encoder_test.rb
+++ test/enigma_encoder_test.rb
@@ -18,4 +18,11 @@ class EnigmaEncoderTest < Minitest::Test
   def test_it_can_set_offset
     assert_equal ["1", "0", "2", "5"], @encoder.offset("040895")
   end
+
+  def test_it_can_set_shift
+    keys = @encoder.keys("02715")
+    offset = @encoder.offset("040895")
+    
+    assert_equal [2, 27, 73, 20], @encoder.shift(keys, offset)
+  end
 end
