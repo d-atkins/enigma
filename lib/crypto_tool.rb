4315e7d0788fb0d127d515930b71c9ec86ffa10e
David Atkins <atki1080@gmail.com>
2020-01-12 14:18:26 -0700

    Copy methods from encoder

--- lib/crypto_tool.rb
+++ lib/crypto_tool.rb
@@ -1,3 +1,23 @@
 class CryptoTool
 
+  def initialize
+    @whitelist = ('a'..'z').to_a << ' '
+  end
+
+  def keys(key)
+    key.split('').each_cons(2).map {|a,b| a + b}
+  end
+
+  def offset(date)
+    (date.to_i**2).to_s[-4..-1].split('')
+  end
+
+  def shifts(keys, offset)
+    keys.zip(offset).map {|key, off| key.to_i + off.to_i }
+  end
+
+  def caesar_shift(character, shift)
+    index = @whitelist.index(character.downcase)
+    index ? @whitelist.rotate(shift)[index] : character
+  end
 end
