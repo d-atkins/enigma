7d44f8447bf755a7385a6a1922bede9e6ef3b1c9
David Atkins <atki1080@gmail.com>
2020-01-12 15:33:52 -0700

    Add case in encrypt for no date given

--- lib/encoder.rb
+++ lib/encoder.rb
@@ -3,6 +3,7 @@ require './lib/crypto_tool'
 class Encoder < CryptoTool
 
   def encrypt(message, key = nil, date = nil)
+    date = today if date.nil?
     shifts = shifts(keys(key), offset(date))
     encryption = shift_all(message, shifts)
     report(:encryption, encryption, key, date)
