bda9ead62d211643141bb38be4d672357d24a621
David Atkins <atki1080@gmail.com>
2020-01-12 15:06:53 -0700

    Remove initialize

--- lib/encoder.rb
+++ lib/encoder.rb
@@ -2,14 +2,6 @@ require './lib/crypto_tool'
 
 class Encoder < CryptoTool
 
-  def initialize
-    @whitelist = ('a'..'z').to_a << ' '
-  end
-
-  # def encryption_info(encryption, key, date)
-  #   {encryption: encryption, key: key, date: date}
-  # end
-
   def encrypt(message, key = nil, date = nil)
     shifts = shifts(keys(key), offset(date))
     encryption = shift_all(message, shifts)
