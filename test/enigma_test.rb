fb40fa637100403325a1c64c6b0db6c62247f75e
David Atkins <atki1080@gmail.com>
2020-01-13 12:14:29 -0700

    Add encrypt tests

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,9 +1,13 @@
-require_relative "test_helper"
-require "./lib/enigma"
+require_relative 'test_helper'
+require './lib/enigma'
+require 'time'
+require 'mocha/minitest'
 
 class EnigmaTest < Minitest::Test
 
   def setup
+    dummy_time = Time.parse("2020-1-12")
+    Time.stubs(:now).returns(dummy_time)
     @enigma = Enigma.new
   end
 
@@ -15,5 +19,36 @@ class EnigmaTest < Minitest::Test
     assert_instance_of Encoder, @enigma.encoder
     assert_instance_of Decoder, @enigma.decoder
     assert_instance_of Cracker, @enigma.cracker
+  end
+
+  def test_it_can_encrypt
+    expected = {
+      encryption: "keder ohulw",
+      key: "02715",
+      date: "040895"
+    }
+
+    assert_equal expected, @enigma.encrypt("hello world", "02715", "040895")
+  end
+
+  def test_it_can_encrypt_with_no_date_argument
+    expected = {
+      encryption: "nib udmcxpu",
+      key: "02715",
+      date: "120120"
+    }
+
+    assert_equal expected, @enigma.encrypt("hello world", "02715")
+  end
+
+  def test_it_can_encrypt_with_no_key_or_date_argument
+    @enigma.encoder.stubs(:random_key).returns("02715")
+    expected = {
+      encryption: "nib udmcxpu",
+      key: "02715",
+      date: "120120"
+    }
+
+    assert_equal expected, @enigma.encrypt("hello world")
   end
 end
