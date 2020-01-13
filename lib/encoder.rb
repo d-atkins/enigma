a8667d7aa4c670d399b29db4fa1c539bf6df72a4
David Atkins <atki1080@gmail.com>
2020-01-12 21:53:05 -0700

    Change random_key to randomize each digit separately

--- lib/encoder.rb
+++ lib/encoder.rb
@@ -3,7 +3,8 @@ require './lib/crypto_tool'
 class Encoder < CryptoTool
 
   def random_key
-    rand.to_s[2..6]
+    key = "00000"
+    key.split('').map {|char| rand(10).to_s}.join
   end
 
   def encrypt(message, key = nil, date = nil)
