cc0e5c9c0a820528136ffa6a26980f7d75c9ad02
David Atkins <atki1080@gmail.com>
2020-01-12 22:41:15 -0700

    Simplify logic

--- lib/decoder.rb
+++ lib/decoder.rb
@@ -4,7 +4,7 @@ class Decoder < CryptoTool
 
   def decrypt(ciphertext, key, date = nil)
     date = today if date.nil?
-    shifts = shifts(keys(key), offset(date)).map{|shift| shift * -1}
+    shifts = shifts(keys(key), offset(date)).map{|shift| -shift}
     decryption = shift_all(ciphertext, shifts)
     report(:decryption, decryption, key, date)
   end
