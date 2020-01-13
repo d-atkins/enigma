d826823be6344fd9dfb4827125fc69a995a2ef08
David Atkins <atki1080@gmail.com>
2020-01-12 22:29:30 -0700

    Add decrypt method

--- lib/decoder.rb
+++ lib/decoder.rb
@@ -1,3 +1,11 @@
-class Decoder
+require './lib/crypto_tool'
 
+class Decoder < CryptoTool
+
+  def decrypt(ciphertext, key, date = nil)
+    date = today if date.nil?
+    shifts = shifts(keys(key), offset(date)).map{|shift| shift * -1}
+    decryption = shift_all(ciphertext, shifts)
+    report(:decryption, decryption, key, date)
+  end
 end
