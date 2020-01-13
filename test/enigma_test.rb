68d5dba9b34390349c385cfbc73ccec19b5c8b75
David Atkins <55907198+d-atkins@users.noreply.github.com>
2020-01-13 13:47:55 -0700

    Revert "Command Line Interface"

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,13 +1,9 @@
-require_relative 'test_helper'
-require './lib/enigma'
-require 'time'
-require 'mocha/minitest'
+require_relative "test_helper"
+require "./lib/enigma"
 
 class EnigmaTest < Minitest::Test
 
   def setup
-    dummy_time = Time.parse("2020-1-12")
-    Time.stubs(:now).returns(dummy_time)
     @enigma = Enigma.new
   end
 
@@ -19,56 +15,5 @@ class EnigmaTest < Minitest::Test
     assert_instance_of Encoder, @enigma.encoder
     assert_instance_of Decoder, @enigma.decoder
     assert_instance_of Cracker, @enigma.cracker
-  end
-
-  def test_it_can_encrypt
-    expected = {
-      encryption: "keder ohulw",
-      key: "02715",
-      date: "040895"
-    }
-
-    assert_equal expected, @enigma.encrypt("hello world", "02715", "040895")
-  end
-
-  def test_it_can_encrypt_with_no_date_argument
-    expected = {
-      encryption: "nib udmcxpu",
-      key: "02715",
-      date: "120120"
-    }
-
-    assert_equal expected, @enigma.encrypt("hello world", "02715")
-  end
-
-  def test_it_can_encrypt_with_no_key_or_date_argument
-    @enigma.encoder.stubs(:random_key).returns("02715")
-    expected = {
-      encryption: "nib udmcxpu",
-      key: "02715",
-      date: "120120"
-    }
-
-    assert_equal expected, @enigma.encrypt("hello world")
-  end
-
-  def test_it_can_decrypt
-    expected = {
-      decryption: "hello world",
-      key: "02715",
-      date: "040895"
-    }
-
-    assert_equal expected, @enigma.decrypt("keder ohulw", "02715", "040895")
-  end
-
-  def test_it_can_encrypt_with_no_date_argument
-    expected = {
-      decryption: "hello world",
-      key: "02715",
-      date: "120120"
-    }
-
-    assert_equal expected, @enigma.decrypt("nib udmcxpu", "02715")
   end
 end
