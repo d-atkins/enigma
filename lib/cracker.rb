3b108ab7cc5883fd61182757be373d0a5da793c2
David Atkins <atki1080@gmail.com>
2020-01-13 21:56:23 -0700

    Override shifts

--- lib/cracker.rb
+++ lib/cracker.rb
@@ -1,3 +1,11 @@
-class Cracker
+require './lib/decoder'
 
+class Cracker < Decoder
+  def shifts(cipher_length, cipher_end, known_end = " end")
+    cipher_end.split('').zip(known_end.split('')).map do |cipher_char, known_char|
+      shift = @whitelist.index(cipher_char) - @whitelist.index(known_char)
+      shift += 27 if shift < 0
+      shift
+    end.rotate(-(cipher_length % 4))
+  end
 end
