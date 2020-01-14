46c3fb76b278e853d70ffc589007b52825c03012
David Atkins <atki1080@gmail.com>
2020-01-14 13:39:39 -0700

    Add reverse_shifts method

--- lib/decoder.rb
+++ lib/decoder.rb
@@ -2,6 +2,10 @@ require './lib/crypto_tool'
 
 class Decoder < CryptoTool
 
+  def reverse_shifts(shifts)
+    shifts.map{|shift| -shift}
+  end
+
   def decrypt(ciphertext, key, date = today)
     shifts = shifts(keys(key), offset(date)).map{|shift| -shift}
     decryption = shift_all(ciphertext, shifts)
