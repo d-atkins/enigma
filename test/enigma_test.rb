90ba0f061fcdb05425b928364656c77d75953468
David Atkins <atki1080@gmail.com>
2020-01-13 22:58:45 -0700

    Fix repeated test name

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -62,7 +62,7 @@ class EnigmaTest < Minitest::Test
     assert_equal expected, @enigma.decrypt("keder ohulw", "02715", "040895")
   end
 
-  def test_it_can_encrypt_with_no_date_argument
+  def test_it_can_decrypt_with_no_date_argument
     expected = {
       decryption: "hello world",
       key: "02715",
